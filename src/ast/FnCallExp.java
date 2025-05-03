404cbb450904959857575f4a7cf276c5eb517501
Ryan <azbo400@gmail.com>
2025-05-03 08:32:04 -0700

    Remove enterprise server links

--- src/ast/FnCallExp.java
+++ src/ast/FnCallExp.java
@@ -1,6 +1,5 @@
 /*
  * This file was adapted from the tinyVars repository provided in class.
- * Original source: https://github.students.cs.ubc.ca/CPSC410-2024W-T2/tinyVars
  * Modifications have been made to implement the FileScript DSL.
  */
 
